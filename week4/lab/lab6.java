6352f6d8b8fd94f52ced83588a5177a3b9bdeca2
Zaviel <81034608+zelvet-velvet@users.noreply.github.com>
2021-03-22 15:48:09 +0800

    Update lab6.java

---
+++ week4/lab/lab6.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sb=new Scanner(System.in);
+    double x, n;
+    double i=0, j=1, k=0, a=0, b=0, s=0;
+    System.out.print("sinx, x= ");
+    x=sb.nextInt();
+    System.out.println("x^n, n= ");
+    n=sb.nextInt();
+    while(true)
+    { 
+      i=
+      for(int f=0;f<j;f++)
+      {
+        k=k*k+1;
+      }
+      a=i*Math.pow(x,n)/k;
+      s=s+a;
+      if(x>0)
+      {
+        i=-1;
+      }
+      else if(x<0)
+      {
+        i=1;
+      }
+      b++;
+      if(b)
+    }
+    System.out.println(s);
+  }
+}
