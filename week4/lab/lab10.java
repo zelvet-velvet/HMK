84bc73d5734adcfb9912be198d4ce902335d30c6
zelvet-velvet <zelvet0522@gmail.com>
2021-03-23 05:03:05 +0800

    @@

---
+++ week4/lab/lab10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class lab10 {
+  public static void main(String[] args) {
+    String[] b={"Ferrari","BMW","Ford","Mazda","FCU"};
+    for(String c:b)
+    {
+      System.out.println(c);
+    }
+  }
+}
